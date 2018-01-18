b136eb95c8847a905e7cd86e8bbaba8923679775
Oscarlight <oscarli2716@gmail.com>
2018-01-18 00:38:16 -0500

    linux

--- transiXNOR/plotting.py
+++ transiXNOR/plotting.py
@@ -8,6 +8,8 @@ Eg = 0.252
 gridx = np.genfromtxt('gridx.out')
 Nx = gridx.shape[0]
 gridy = np.genfromtxt("gridy.out")
+
+Vds=0.3; Vbg=0.0; Vtg=0.0
 ## fixed charge
 if (PLOT_FIXED_CHARGE):
 	fn_fixed_charge = 'fixed_charge.out'
@@ -19,7 +21,7 @@ if (PLOT_FIXED_CHARGE):
 	
 ## band diagram
 if (PLOT_BAND):
-	fn_band_diag = 'datiout/Phi0.0.out'
+	fn_band_diag = 'datiout/phi_%s_%s_%s.out'%(Vds, Vbg, Vtg)
 	band_diag = np.genfromtxt(fn_band_diag)
 	band_diag = np.reshape(band_diag, (-1, Nx))
 	print(band_diag.shape)
@@ -28,7 +30,7 @@ if (PLOT_BAND):
 	plt.show()
 
 if (PLOT_TRAN):
-	fn_tran = 'datiout/T0.2.out'
+	fn_tran = 'datiout/T_%s_%s_%s.out'%(Vds, Vbg, Vtg)
 	energy_tran = np.genfromtxt(fn_tran,delimiter=' ')
 	energy_tran = energy_tran[energy_tran[:,1] > 0.0, :]
 	print(energy_tran.shape)
@@ -36,7 +38,7 @@ if (PLOT_TRAN):
 	plt.show()
 
 if (PLOT_CHARGE):
-	fn_charge = 'datiout/ncar0.0.out'
+	fn_charge = 'datiout/ncar_%s_%s_%s.out'%(Vds, Vbg, Vtg)
 	charge_density = np.genfromtxt(fn_charge)
 	charge_density = np.reshape(charge_density, (-1, Nx))
 	plt.semilogy(gridy, np.abs(charge_density[:,Nx/2]))
