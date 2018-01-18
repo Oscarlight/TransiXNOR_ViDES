d98b6f8494c835da89e8ec3896fb74b714a25d18
OscarLight <oscarli2716@gmail.com>
2018-01-17 23:55:29 -0500

    Bi2Se3

--- transiXNOR/plotting.py
+++ transiXNOR/plotting.py
@@ -4,25 +4,27 @@ PLOT_FIXED_CHARGE = True
 PLOT_BAND = True
 PLOT_TRAN = False
 PLOT_CHARGE = True
+Eg = 0.252
 gridx = np.genfromtxt('gridx.out')
+Nx = gridx.shape[0]
 gridy = np.genfromtxt("gridy.out")
 ## fixed charge
 if (PLOT_FIXED_CHARGE):
 	fn_fixed_charge = 'fixed_charge.out'
 	fixed_charge = np.genfromtxt(fn_fixed_charge)
-	fixed_charge = np.reshape(fixed_charge, (-1, 11))
+	fixed_charge = np.reshape(fixed_charge, (-1, Nx))
 	print(fixed_charge.shape)
-	plt.plot(gridy, fixed_charge[:,5])
+	plt.plot(gridy, fixed_charge[:,Nx/2])
 	plt.show()
 	
 ## band diagram
 if (PLOT_BAND):
 	fn_band_diag = 'datiout/Phi0.0.out'
 	band_diag = np.genfromtxt(fn_band_diag)
-	band_diag = np.reshape(band_diag, (-1, 11))
+	band_diag = np.reshape(band_diag, (-1, Nx))
 	print(band_diag.shape)
-	plt.plot(gridy, band_diag[:,5])
-	plt.plot(gridy, band_diag[:,5]-0.93)
+	plt.plot(gridy, band_diag[:,Nx/2])
+	plt.plot(gridy, band_diag[:,Nx/2]-Eg)
 	plt.show()
 
 if (PLOT_TRAN):
@@ -36,6 +38,6 @@ if (PLOT_TRAN):
 if (PLOT_CHARGE):
 	fn_charge = 'datiout/ncar0.0.out'
 	charge_density = np.genfromtxt(fn_charge)
-	charge_density = np.reshape(charge_density, (-1, 11))
-	plt.semilogy(gridy, np.abs(charge_density[:,5]))
+	charge_density = np.reshape(charge_density, (-1, Nx))
+	plt.semilogy(gridy, np.abs(charge_density[:,Nx/2]))
 	plt.show()
