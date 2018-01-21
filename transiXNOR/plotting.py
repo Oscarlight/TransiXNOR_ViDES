59907e759f93f99f52c28ce4f2c0d66b9dd28417
OscarLight <oscarli2716@gmail.com>
2018-01-21 11:22:35 -0500

    check tunneling window vs. bandgap

--- transiXNOR/plotting.py
+++ transiXNOR/plotting.py
@@ -58,8 +58,10 @@ if (PLOT_TRAN or PLOT_CURRENT_SPECTRUM):
 	T = energy_tran[:, 1]
 	if (PLOT_CURRENT_SPECTRUM):
 		jE = 2*q*q/(2*pi*hbar)*T*(Fermi((E-0)/vt)-Fermi((E-Vds)/vt))
-		plt.semilogy(E, jE)
-		plt.savefig('current_spectrum.png')
+		print(jE.shape)
+		print(E.shape)
+		# plt.semilogy(E, jE)
+		# plt.savefig('current_spectrum.png')
 	if (PLOT_TRAN):
 		print(energy_tran.shape)
 		plt.semilogy(energy_tran[:,0], energy_tran[:,1])
