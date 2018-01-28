8ea59fb3c7a6f77176ef189a4306a4837fa2680d
OscarLight <oscarli2716@gmail.com>
2018-01-28 16:09:24 -0500

    transiXNOR Modeling

--- transiXNOR/module_Bi2Se3.py
+++ transiXNOR/module_Bi2Se3.py
@@ -103,28 +103,9 @@ class Bi2Se3:
             h[:slices+1:2,2]  = self.E0 + self.Egap/2 + self.coeff_Ec * k * k;
             h[0][2] = 0
             h[1:slices+1:2,2] = self.E0 - self.Egap/2 - self.coeff_Ev * k * k;
-            h[slices+1::2,2]  = 1j * hbar * self.vf * k / q;
+            # h[slices+1::2,2]  = 1j * hbar * self.vf * k / q;
             h[slices+2::4,2]  = self.thop_elec+self.thop_elec*exp(k*self.delta*1j);
             h[slices+4::4,2]  = self.thop_elec+self.thop_elec*exp(-k*self.delta*1j);
-            flaggo=0;
-            # kk=1;
-            # for ii in range(slices+1,2*slices):
-            #     if ((ii%2)==0):
-            #         h[ii][0]=kk;
-            #         h[ii][1]=kk+1;
-            #         if ((flaggo%2)==0):
-            #             if ((self.y[kk-1]>=self.ymin)&(self.y[kk-1]<=self.ymax)):
-            #                 h[ii][2]=self.thop_elec+self.thop_elec*exp(k*self.delta*1j);
-            #             else:
-            #                 h[ii][2]=self.thop+self.thop*exp(k*self.delta*1j);
-            #         else:
-            #             if ((self.y[kk-1]>=self.ymin)&(self.y[kk-1]<=self.ymax)):
-            #                 h[ii][2]=self.thop_elec+self.thop_elec*exp(-k*self.delta*1j);
-            #             else:
-            #                 h[ii][2]=self.thop+self.thop*exp(-k*self.delta*1j);
-
-            #         flaggo=flaggo+1;
-            #     kk=kk+1;
 
             H.Eupper = self.Eupper;
             H.Elower = self.Elower;
