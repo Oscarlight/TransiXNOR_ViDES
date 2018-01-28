5edbf8d2b30a3646da54da22570f1d232c6d8dd4
OscarLight <oscarli2716@gmail.com>
2018-01-28 16:28:43 -0500

    transiXNOR Modeling

--- transiXNOR/module_Bi2Se3.py
+++ transiXNOR/module_Bi2Se3.py
@@ -105,8 +105,8 @@ class Bi2Se3:
             h[:slices+1:2,2]  = self.BC_MX2 + self.coeff_Ec * k * k;
             h[0][2] = 0
             h[1:slices+1:2,2] = self.BV_MX2 - self.coeff_Ev * k * k;
-            h[slices+1::2,2]  = 1j * hbar * self.vf * k / q;
-            # h[slices+1::2,2]  = self.thop_elec;
+            # h[slices+1::2,2]  = 1j * hbar * self.vf * k / q;
+            h[slices+1::2,2]  = self.thop_elec;
             h[slices+2::4,2]  = self.thop_elec+self.thop_elec*exp(k*self.delta*1j);
             h[slices+4::4,2]  = self.thop_elec+self.thop_elec*exp(-k*self.delta*1j);
 
