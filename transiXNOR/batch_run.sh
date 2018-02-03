3c2eb472acc996c6a090b002571f53b87dd87a45
OscarLight <oscarli2716@gmail.com>
2018-02-02 22:21:10 -0500

    BiSe transiXNOR

--- transiXNOR/batch_run.sh
+++ transiXNOR/batch_run.sh
@@ -1,13 +1,42 @@
 version=D9
-for vds in 0.4 
-	do
-		# python device.py $version --vdsmin $vds --vdsmax $vds --vdsN 1 &
-		# let vds-=0.01
-		# python device.py $version --vdsmin $vds --vdsmax $vds --vdsN 1 &
-		# let vds-=0.01
-		# python device.py $version --vdsmin $vds --vdsmax $vds --vdsN 1 &
-		let vds-=0.01
-		python device.py $version --vdsmin $vds --vdsmax $vds --vdsN 1
-		let vds-=0.01
-		wait
-	done
+python device.py $version --vdsmin 0.30 --vdsmax 0.30 --vdsN 1 &
+python device.py $version --vdsmin 0.29 --vdsmax 0.29 --vdsN 1 &
+python device.py $version --vdsmin 0.28 --vdsmax 0.28 --vdsN 1 & 
+python device.py $version --vdsmin 0.27 --vdsmax 0.27 --vdsN 1 
+python device.py $version --vdsmin 0.26 --vdsmax 0.26 --vdsN 1 &
+python device.py $version --vdsmin 0.25 --vdsmax 0.25 --vdsN 1 &
+python device.py $version --vdsmin 0.24 --vdsmax 0.24 --vdsN 1 & 
+python device.py $version --vdsmin 0.23 --vdsmax 0.23 --vdsN 1 
+python device.py $version --vdsmin 0.22 --vdsmax 0.22 --vdsN 1 &
+python device.py $version --vdsmin 0.21 --vdsmax 0.21 --vdsN 1 &
+python device.py $version --vdsmin 0.20 --vdsmax 0.20 --vdsN 1 &
+python device.py $version --vdsmin 0.19 --vdsmax 0.19 --vdsN 1 
+python device.py $version --vdsmin 0.18 --vdsmax 0.18 --vdsN 1 &
+python device.py $version --vdsmin 0.17 --vdsmax 0.17 --vdsN 1 &
+python device.py $version --vdsmin 0.16 --vdsmax 0.16 --vdsN 1 &
+python device.py $version --vdsmin 0.15 --vdsmax 0.15 --vdsN 1 
+python device.py $version --vdsmin 0.14 --vdsmax 0.14 --vdsN 1 &
+python device.py $version --vdsmin 0.13 --vdsmax 0.13 --vdsN 1 &
+python device.py $version --vdsmin 0.12 --vdsmax 0.12 --vdsN 1 &
+python device.py $version --vdsmin 0.11 --vdsmax 0.11 --vdsN 1 
+python device.py $version --vdsmin 0.10 --vdsmax 0.10 --vdsN 1 & 
+python device.py $version --vdsmin 0.09 --vdsmax 0.09 --vdsN 1 &
+python device.py $version --vdsmin 0.08 --vdsmax 0.08 --vdsN 1 &
+python device.py $version --vdsmin 0.07 --vdsmax 0.07 --vdsN 1
+python device.py $version --vdsmin 0.06 --vdsmax 0.06 --vdsN 1 &
+python device.py $version --vdsmin 0.05 --vdsmax 0.05 --vdsN 1 &
+python device.py $version --vdsmin 0.04 --vdsmax 0.04 --vdsN 1 &
+python device.py $version --vdsmin 0.03 --vdsmax 0.03 --vdsN 1
+python device.py $version --vdsmin 0.02 --vdsmax 0.02 --vdsN 1 &
+python device.py $version --vdsmin 0.01 --vdsmax 0.01 --vdsN 1 &
+python device.py $version --vdsmin 0.00 --vdsmax 0.00 --vdsN 1 &
+python device.py $version --vdsmin -0.01 --vdsmax -0.01 --vdsN 1
+python device.py $version --vdsmin -0.02 --vdsmax -0.02 --vdsN 1 &
+python device.py $version --vdsmin -0.03 --vdsmax -0.03 --vdsN 1 &
+python device.py $version --vdsmin -0.04 --vdsmax -0.04 --vdsN 1 &
+python device.py $version --vdsmin -0.05 --vdsmax -0.05 --vdsN 1
+python device.py $version --vdsmin -0.06 --vdsmax -0.06 --vdsN 1 &
+python device.py $version --vdsmin -0.07 --vdsmax -0.07 --vdsN 1 &
+python device.py $version --vdsmin -0.08 --vdsmax -0.08 --vdsN 1 &
+python device.py $version --vdsmin -0.09 --vdsmax -0.09 --vdsN 1
+python device.py $version --vdsmin -0.10 --vdsmax -0.10 --vdsN 1
