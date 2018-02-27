615996fd9ca81e1f079d981b363d6e21f1b16453
FrankLeeX <330952489@qq.com>
2018-02-27 17:38:34 -0500

    Changing calculation of C and comment out save every charge

--- transiXNOR/plotting.py
+++ transiXNOR/plotting.py
@@ -360,14 +360,11 @@ if (QV_CALCULATION):
 					np.abs(gridx[Nx / 2] - gridx[Nx - 1]) * 1e-9)  # C/m^2
 				Q_top.append(Q_top_x)
 				Q_bottom.append(Q_bottom_x)
-		np.save(model_path + '/charges/TG_Q_' + voltage, Q_top)  # save top gate charges
-		np.save(model_path + '/charges/BG_Q_' + voltage, Q_bottom)  # save bottom gate charges
+		# np.save(model_path + '/charges/TG_Q_' + voltage, Q_top)  # save top gate charges
+		# np.save(model_path + '/charges/BG_Q_' + voltage, Q_bottom)  # save bottom gate charges
 		Q_top_total = np.sum(Q_top)  # C/m^2
 		Q_bottom_total = np.sum(Q_bottom)  # C/m^2
-		if (V != 0.0):
-			C = (Q_top_total + Q_bottom_total) / (Vtg + Vbg)
-		else:
-			C = 0.175 # Because all calcualted C are close to 0.175
+		C = er*eps0/(1e-9)
 		Q_top_real = C * Vtg - Q_bottom_total
 		Q_bottom_real = Q_bottom_total - C * Vbg
 		return Q_top_real, Q_bottom_real
